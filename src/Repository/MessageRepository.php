35d96d2c28f14d1398734f1b9aceb6ec263e8bb0
masaoikawa <masao.ikawa@me.com>
2020-04-04 22:30:15 +0900

    初めのコミット

---
+++ src/Repository/MessageRepository.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Message;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Symfony\Bridge\Doctrine\RegistryInterface;
+use Doctrine\ORM\Tools\Pagination\Paginator;
+
+/**
+* @method Message|null find($id, $lockMode = null, $lockVersion = null)
+* @method Message|null findOneBy(array $criteria, array $orderBy = null)
+* @method Message[]    findAll()
+* @method Message[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+*/
+class MessageRepository extends ServiceEntityRepository
+{
+    public function __construct(RegistryInterface $registry)
+    {
+        parent::__construct($registry, Message::class);
+    }
+
+    public function findAll()
+    {
+        return $this->findBy(array(), array('posted' => 'DESC'));
+    }
+
+    public function getPage($currentPage = 1, $limit = 5)
+    {
+        $query = $this->createQueryBuilder('p')
+            ->orderBy('p.posted','DESC')
+            ->getQuery();
+        $paginator = $this->paginate($query, $currentPage, $limit);
+        return $paginator;
+    }
+
+    public function paginate($dql, $page=1, $limit=5)
+    {
+        $paginator = new Paginator($dql);
+        $paginator->getQuery()
+            ->setFirstResult($limit * ($page - 1))
+            ->setMaxResults($limit);
+        return $paginator;
+    }
+}
